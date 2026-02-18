ac1e996cb0a7e1798df6360ae3589adc50dc95d7
Caio Filardis <caiosantosshake@hotmail.com>
2026-02-17 22:42:53 -0300

    Adiciona itens no localstorage

--- js/carrinho.js
+++ js/carrinho.js
@@ -6,13 +6,44 @@ const botoesAdicionarAoCarrinho = document.querySelectorAll('.adicionar-ao-carri
 
 /*  passo 2 - adicionar uma evento de escuta nesses botões pra quando clicar disparar uma ação */
 botoesAdicionarAoCarrinho.forEach(botao => {
-    botao.addEventListener('click', () => {
+    botao.addEventListener('click', (evento) => {
         // passo 3 - pegar o valor do contador do carrinho e adicionar +1
-        const elementoProduto = event.target.closest('.produto'); // pega o elemento mais próximo com a classe 'produto'
+        const elementoProduto = evento.target.closest(".produto") // pega o elemento mais próximo com a classe 'produto'
         const produtoId = elementoProduto.dataset.id; // pega o valor do atributo 'data-id' do elemento produto
-        const produtoNome = elementoProduto.querySelector('.produto-nome').textContent; // pega o nome do produto
-        const produtoImagem = elementoProduto.querySelector('.img').src; // pega a URL da imagem do produto
-        const produtoPreco = parseFloat(elementoProduto.querySelector('.preco').textContent.replace('R$', '').replace(".", "").replace(",",".").trim()); // pega o preço do produto e converte para número
+        const produtoNome = elementoProduto.querySelector(".nome").textContent; // pega o nome do produto
+        const produtoImagem = elementoProduto.querySelector("img").getAttribute("src"); // pega a URL da imagem do produto
+        const produtoPreco = parseFloat(elementoProduto.querySelector(".preco").textContent.replace('R$', '').replace(".", "").replace(",",".").trim()); // pega o preço do produto e converte para número
         
+        // 1. bsucar lista de itens no carrinho
+        const carrinho = obterItensDocCarrinho();
+
+        // 2. verifica se existe itens no carrinho
+        const existeItem = carrinho.find(produto => produto.id === produtoId);
+
+        // 2.1. se existe produto
+        if (existeItem) {
+            existeItem.quantidade += 1;
+        } else { 
+            // se não existe adiciono o produto com quantidade + 1
+            const produto = {
+                id: produtoId,
+                name: produtoNome,
+                imagem: produtoImagem,
+                preco: produtoPreco,
+                quantidade: 1
+            };
+            carrinho.push(produto);
+        }
+
+        salvarProdutoCarrinho(carrinho);
     });
 });
+
+function salvarProdutoCarrinho(carrinho) {
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+}
+
+function obterItensDocCarrinho() {
+    const produtos = localStorage.getItem("carrinho");
+    return produtos ? JSON.parse(produtos):[]; // transforma em objeto
+}
